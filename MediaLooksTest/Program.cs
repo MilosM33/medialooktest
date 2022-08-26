2ffab90f3120d7f323ab4f77a882f1b5158eb366
Milos <61538921+MilosM33@users.noreply.github.com>
2022-08-26 15:40:52 +0200

    Half duplex webrtc

--- MediaLooksTest/Program.cs
+++ MediaLooksTest/Program.cs
@@ -10,8 +10,33 @@ namespace MediaLooksTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+            MWriterClass writer = new MWriterClass();
+            MWebRTCClass webRTC = new MWebRTCClass();
+            MSendersClass mSendersClass = new MSendersClass();
+            MFileClass mFile = new MFileClass();
+            Random r = new Random();
+
+            const string signalingStr = "https://rtc.medialooks.com:8889";
+
+            String url = (signalingStr + "/Room" + +r.Next(1000) + "/Streamer" + r.Next(1000));
+            String pbsId = "";
+            mFile.FileNameSet(@"C:\Users\milos\programy\Programovanie\Wezeo\MediaLooksTest\MediaLooksTest\Videos\video1.mp4", "");
+            mFile.PropsSet("loop", "true");
+            mFile.FilePlayStart();
+            mFile.PluginsAdd(webRTC, 10);
+
+
+
+            // Stream
+            // Half duplex
+            webRTC.PropsSet("mode", "sender");
+            webRTC.Login(url, "", out pbsId);
+            
+
+            Console.WriteLine(url);
+
             Console.ReadLine();
+            webRTC.Logout();
         }
     }
 }
